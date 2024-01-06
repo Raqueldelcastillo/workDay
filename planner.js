cf63c780664bdfbbefc9b5378b95ed21d64abc7e
rdelcastillo <117642803+Raqueldelcastillo@users.noreply.github.com>
2024-01-06 22:21:36 +0000

    Add files via upload

---
+++ planner.js
@@ -0,0 +1,12 @@
+//Figure out today's date, and put it at the top of the page.
+
+
+//Figure out how to make a block ofr every hour from 9am to 5pm inclusive.
+
+//Each block should allow for text entry (perhaps the block in an input element/textarea)
+
+//Each block should include an hour label
+
+//Each block should include a save button
+
+//
