e78ebf963f32692ef6a6907f757c7a8028662c56
Raquel del Castillo <117642803+Raqueldelcastillo@users.noreply.github.com>
2024-01-07 22:17:38 +0000

    add timebox

--- planner.js
+++ planner.js
@@ -1,6 +1,32 @@
 //Figure out today's date, and put it at the top of the page.
 
+$(document).ready(function () {
+  function displayTime() {
+    var rightNow = dayjs().format("DD MMM YYYY [at] hh:mm:ss a");
+    $("#currentDay").text(rightNow);
+  }
+  displayTime();
 
+  //we want to create an event listener for all of the  save buttons
+  // when someone clicks on the button
+  $(".saveButton").on("click", function () {
+    // we want to take the value of the textarea (using .siblings('.class-name-here').val())
+    let userValue = $(this).siblings(".description").val();
+    let userTime = $(this).parent().attr("id");
+
+    localStorage.setItem(userTime, userValue);
+  });
+  // then we want to save that value to localStorage (localStorage.setItem('name', value))
+
+  // this is how we can use siblings
+  // $("li.start").siblings().css({ color: "red", border: "2px solid red" });
+
+  // when someone loads the page
+  // if there is data in localStorage
+  // ex: localStorage.getItem('9AM')
+  $('#desc-9').val(localStorage.getItem('hour-9'))
+  // we can display that data to the page
+});
 //Figure out how to make a block ofr every hour from 9am to 5pm inclusive.
 
 //Each block should allow for text entry (perhaps the block in an input element/textarea)
